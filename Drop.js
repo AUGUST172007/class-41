0c15c0c23c1456674feea7caec0ebd2a23dcc174
AUGUST172007 <71178819+AUGUST172007@users.noreply.github.com>
2020-12-16 21:11:09 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,52 @@
+class Drop{
+constructor(x,y){
+    var options = {
+        'restitution':0.1,
+        'friction':0.1,
+        'density':1.0
+    }  
+this.body = Bodies.circle(x,y,5)
+this.radius = 5;
+World.add(world,this.body)
+
+}
+
+
+
+
+
+
+update(){
+if(this.body.position.y > 800){
+
+matter.Body.setPosition(this.body,{
+x : random(0,400),
+y : random(0,400)
+
+  })
+ }
+}
+display(){
+    var pos =this.body.position;
+    Fill("blue");
+      ellipseMode(CENTER);
+  ellipse(pos.x,pos.y,this.radius,this.radius)
+  
+  
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
